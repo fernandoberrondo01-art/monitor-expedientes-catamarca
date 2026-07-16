f63e00952e6cebc58164bd3b52f276aaa8dd764d
Fernando Berrondo <fernandoberrondo01@gmail.com>
2026-07-16 12:57:07 -0300

    Diagnóstico Telegram

--- telegram_utils.py
+++ telegram_utils.py
@@ -10,6 +10,9 @@ CHAT_ID = os.getenv("CHAT_ID")
 
 def enviar_mensaje(texto):
 
+    print("BOT_TOKEN cargado:", BOT_TOKEN is not None)
+    print("CHAT_ID:", CHAT_ID)
+
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
     respuesta = requests.post(
@@ -20,5 +23,8 @@ def enviar_mensaje(texto):
         },
         timeout=30
     )
+
+    print("Código HTTP:", respuesta.status_code)
+    print("Respuesta Telegram:", respuesta.text)
 
     return respuesta.status_code == 200
